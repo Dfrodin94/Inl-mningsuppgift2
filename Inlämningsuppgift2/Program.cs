74b6a376ea779aa515737e292c5c86c4f8ee525e
David Frödin <dawid.frodin@gmail.com>
2020-12-04 20:11:37 +0100

    2 alternativ i menyn kvar. Gjort en static klass, känns spontant bra men finns säkert en orsak till varför man vill ha metoder i sin klass, Får se hur jag gör i slutändan.

--- Inlämningsuppgift2/Program.cs
+++ Inlämningsuppgift2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Channels;
 
 namespace Inlämningsuppgift2
@@ -8,6 +10,7 @@ namespace Inlämningsuppgift2
     {
 
         private string name;
+        private string town;
         private int age;
         private string housing;
         private string family;
@@ -18,18 +21,29 @@ namespace Inlämningsuppgift2
         private string favoriteMusic;
         private string motProg;
 
+        public KlassKamrat()
+        {
+
+        }
+
 
         public string Name
         {
             get { return name; }
             set { name = value; }
         }
-        public int Age 
+
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+        public int Age
         {
             get { return age; }
             set { age = value; }
         }
-        
+
         public string Housing
         {
             get { return housing; }
@@ -79,12 +93,219 @@ namespace Inlämningsuppgift2
         }
 
 
-    } 
+
+
+
+    }
+
+    class BasGrupp : KlassKamrat
+    {
+
+        public BasGrupp()
+        {
+
+        }
+
+        public BasGrupp(string name, string town, int age, string housing, string family, string housePet, string latestWork, string hobby, string favFood, string favMusic, string motProg)
+        {
+
+            Name = name;
+            Town = town; 
+            Age = age;
+            Housing = housing;
+            Family = family;
+            HousePet = housePet;
+            Hobby = hobby;
+            LatestWork = latestWork;
+            FavoriteFood = favFood;
+            FavoriteMusic = favMusic;
+            MotProg = motProg;
+        }
+
+        public void ConsoleAllInfo()
+        {
+
+            Console.WriteLine($"{Name}\n{Town}\n{Age}\n{Housing}\n{Family}\n{HousePet}\n{Hobby}\n{LatestWork}\n{FavoriteFood}\n{FavoriteMusic}\n{MotProg}\n");
+        }
+    }
+
+    static class Functions
+    {
+
+        public static void FillGroupInfo(List<BasGrupp> l)
+        {
+
+            l.Add(new BasGrupp("David Josef Frödin", "Sundbyberg", 25, "Lägenhet", "Det vanliga", "Inga", "Verksamhetschef", "Filosof", "Ryskt", "Rock", "blandning mellan kreativtet och kul"));
+            l.Add(new BasGrupp("Mikael Alexander Larsson", "Vänersborg", 36, "Villa", "Fru och 3 döttrar", "Två kaniner", "Resturangledare", "Träning och hälsa", "Bönchiligryta", "elektroniskt", "Daglig hjärngympa"));
+            l.Add(new BasGrupp("Karin Madeleine Karlsson", "Stockholm", 35, "Villa", "Sambo och en dotter", "Tre katter", "Ekonomiassistent", "Bakning och spel", "´Kött och rotfrukter", "!= barnvisor", "Ballt hu det skapas osv"));
+            l.Add(new BasGrupp("Sammy On Tat Wong", "Åmål", 54, "Villa", "Fru och en son", "3 katter och 1 hund", "Personlig assistent", "Fotografering", "Italeinsk", "Soul", "Kunna skapa något"));
+            l.Add(new BasGrupp("Rauf Karimli", "Göteborg", 36, "Villa", "Fru, son och en dotter", "inga", "projektledare", "spela piano", "grillat kött", "gott och blandat", "gränslösa möjligheter att utvecklas"));
+            l.Add(new BasGrupp("Erik Rindlert", "Stockholm", 31, "lägenhet", "Ogift", "inga", "IT-support", "Litteratur", "Fisksoppa", "Darkthrone", "Bygga bygga bygga"));
+            l.Add(new BasGrupp("Daniel Jacob Arlbring", "Alingsås", 24, "Lägenhet", "Sambo", "Inga", "Väktare", "Motorsport", "Italienskt", "house", "okänt"));
+            l.Add(new BasGrupp("Svante Joelsson", "Strömstad", 36, "Lägenhet", "Sambo och en son", "Hund", "Fritidspedagog", "Illustrera", "Pasta al fungi", "Gubbrock", "Kul combo av problemlösning"));
+            l.Add(new BasGrupp("Sarah Winroth", "Vara", 31, "Lägenhet", "Gift", "Inga", "Reseledare", "Motionerar gärna", "All mat", "Allt", "Problemlösning"));
+            l.Add(new BasGrupp("Farzane Zafarzade", "Karlstad", 32, "Lägenhet", "Man", "inga", "IT-support", "Träning", "Pastarätter", "Lugn musik", "Ballt"));
+        }
+
+        public static void Startup (List<BasGrupp> listBaseGroup)
+        {
+
+            Console.WriteLine("Hej och välkommen till hemligheternas kammare, skriv vänligen in lösenordet");
+            String input;
+            bool keepGoing = false;
+
+            while (true)
+            {
+
+
+                input = Console.ReadLine();
+
+
+                if (input == "coffencode")
+                {
+                    keepGoing = true;
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("du skrev fel lösenord, vänligen pröva igen");
+                }
+
+            }
+
+            Console.WriteLine("Snyggt jobbat! Du är inne i hemligheternas kammare");
+
+
+            while (keepGoing)
+            {
+
+                Console.WriteLine("1. Skriv ut namnet på alla i din grupp");
+                Console.WriteLine("2. Information om varje deltagare");
+                Console.WriteLine("3. Sammanlagda åldern av alla deltagare");
+                Console.WriteLine("4. Ta bort en i din grupp");
+                Console.WriteLine("q. Avsluta programmet");
+                Console.WriteLine("\n" + "INFORMATION: Tryck enter för att rensa fönstret");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Functions.AllNamesGroup(listBaseGroup);
+                        break;
+                    case "2":
+                        Functions.DecisionGroupInfo(listBaseGroup);
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "":
+                        Console.Clear();
+                        break;
+                    case "q":
+                        keepGoing = false;
+                        break;
+
+                }
+
+
+
+            }
+
+        }
+
+        public static void ConsoleAllInfoList(List<BasGrupp> l)
+        {
+            foreach (BasGrupp b in l)
+            {
+                b.ConsoleAllInfo();
+            }
+
+        }
+        public static void DecisionGroupInfo (List<BasGrupp> l)
+        {
+
+            string input;
+
+            Console.WriteLine("Vill du ha information om en specifik deltagare eller allihopa?" + "\n");
+
+            // du kan skriva ut alla namn här. 
+
+            Console.WriteLine("1. Skriv deltagarens namn för att skriva ut en specifik deltagare");
+            Console.WriteLine("2. Skriv 'alla' för att allihopa");
+            input = Console.ReadLine();
+            
+
+            if (input == "alla")
+            {
+                Functions.ConsoleAllInfoList(l);
+            }
+            else
+            {
+                Functions.OnePersonInfo(input, l);
+            }
+
+        }
+
+        public static void OnePersonInfo(string input, List<BasGrupp> l)
+        {
+            BasGrupp temp;
+            bool noName = true;
+
+            for (int i = 0; i < l.Count(); i++)
+            {
+                temp = l[i];
+                if (input.Equals(temp.Name))
+                {
+                    temp.ConsoleAllInfo();
+                    noName = true;
+                    break;
+                }
+
+            }
+
+            if (noName)
+            {
+                Console.WriteLine("Namnet finns inte, pröva vänligen igen");
+            }
+        }
+
+        public static void AllNamesGroup(List<BasGrupp> l)
+        {
+
+            List<string> listNames = new List<string>();
+            int i = 0;
+            foreach (BasGrupp b in l)
+            {
+                i++;
+                listNames.Add(b.Name);
+            }
+
+            Console.WriteLine(String.Join(", ", listNames));
+            Console.WriteLine($"Det är totalt {l.Count()} personer i din grupp" + "\n");
+
+        }
+
+    }
 
     class Program
     {
         static void Main(string[] args)
+
         {
+            List<BasGrupp> listBaseGroup = new List<BasGrupp>();
+            Functions.FillGroupInfo(listBaseGroup);
+            Functions.Startup(listBaseGroup);
+
+            /*
+            foreach (BasGrupp b in listBaseGroup)
+            {
+                b.ConsoleAllInfo(); 
+            } 
+            */
+
+            
 
         }
     }
